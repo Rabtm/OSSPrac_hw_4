6e88d309eade3dd7218c9f4c885652b656cd0669
Rabtm <minseok0412@naver.com>
2022-10-03 19:39:24 +0900

    solve merge conflict

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
 name = input("이름을 입력하세요: ")
 num = int(input("학번을 입력하세요: "))
 major = input("학과를 입력하세요: ")
-<<<<<<< HEAD
-year = input("학년를 입력하세요: ")
-=======
 school = input("학교를 입력하세요: ")
->>>>>>> 547dba397e408c4408e27209a21dc276b62a813a
-
+year = input("학년을 입력하세요: ")
 
 print()
 print("<출력>")
 print("이름:", name)
 print("학번:", num)
 print("학과:", major)
-<<<<<<< HEAD
-print("학년:", year)
-=======
 print("학교:", school)
->>>>>>> 547dba397e408c4408e27209a21dc276b62a813a
+print("학년:", year)
+
+
 
